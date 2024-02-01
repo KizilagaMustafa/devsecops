ef558b39f4d288b5a5bb5679df68555029ae13ef
Mustafa Kızılağa <02205076029@ogr.inonu.edu.tr>
2024-02-01 22:38:19 +0300

    project commit

---
+++ service.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Ana sayfa, kullanıcıya iki sayıyı girmesi için bir form gösterir
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Formdan gelen sayıları topla ve sonucu göster
+app.post('/topla', (req, res) => {
+  const { sayi1, sayi2 } = req.body;
+
+  if (isNaN(sayi1) || isNaN(sayi2)) {
+    res.render('sonuc', { hata: 'Geçersiz sayılar' });
+  } else {
+    const toplam = Number(sayi1) + Number(sayi2);
+    res.render('sonuc', { toplam });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu http://localhost:${port} üzerinde çalışıyor`);
+});
